0b173828bf6cbbef8b998ae215aa63184f91419b
Sean <seahnnnnnn@gmail.com>
2023-08-16 21:45:37 +0900

    feat: Temp screen 추가

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import Login2 from "@screens/LoginSignup/Login2";
 import ChangePW1 from "@screens/LoginSignup/ChangePW1";
 import Signup1 from "@screens/LoginSignup/Signup1";
 
+import Temp from "@screens/Main/Temp";
+
 const Stack = createNativeStackNavigator();
 
 const App = () => {
@@ -18,6 +20,7 @@ const App = () => {
         <Stack.Screen name="Login2" component={Login2} />
         <Stack.Screen name="ChangePW1" component={ChangePW1} />
         <Stack.Screen name="Signup1" component={Signup1} />
+        <Stack.Screen name="Temp" component={Temp} />
       </Stack.Navigator>
     </NavigationContainer>
   );
