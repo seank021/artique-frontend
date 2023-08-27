91383d930409cea01413277a29d1afddfbcc3cc7
Sean <seahnnnnnn@gmail.com>
2023-08-27 15:54:59 +0900

    feat: Profile 폴더 구조 생성

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function Profile() {
     return (
         <View>
-            <Text>Profile</Text>
+            <Text>Profile 스크린</Text>
         </View>
     );
 }
