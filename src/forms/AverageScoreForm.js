da4f659d64e40838c4abbaba231ef2ad07a31d7e
Sean <seahnnnnnn@gmail.com>
2023-09-03 17:39:49 +0900

    fix: 간격 수정

--- src/forms/AverageScoreForm.js
+++ src/forms/AverageScoreForm.js
@@ -10,7 +10,7 @@ export default function AverageScoreForm(props) {
     return (
         <View style={tw`flex flex-col w-[90%] self-center`}>
             <Text style={tw`text-[#191919] text-base font-medium mb-[10px]`}>평균 별점</Text>
-            <View style={tw`flex-row items-center`}>
+            <View style={tw`flex-row items-center mb-[9px]`}>
                 <Image source={require("@images/star.png")} style={tw`w-[16px] h-[16px] mr-[3px]`}></Image>
                 <Text style={tw`text-[#191919] text-sm font-medium`}>{props.averageScore}</Text>
             </View>
