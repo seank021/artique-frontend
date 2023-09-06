7cef61ece3d2dad81893596567d006d8d47fbcdd
Sean <seahnnnnnn@gmail.com>
2023-09-06 19:54:55 +0900

    fix: 줄거리 띄어쓰기 수정

--- src/forms/StoryForm.js
+++ src/forms/StoryForm.js
@@ -10,7 +10,7 @@ export default function StoryForm(props) {
         <View style={tw`flex flex-col w-[90%] self-center`}>
             <Text style={tw`text-[#191919] text-base font-medium mb-[10px]`}>줄거리</Text>
             <View style={{ flexDirection: 'row', alignItems: 'baseline', }}>
-                <Text style={tw`text-[#191919] text-sm text-justify leading-6`}> {isExpanded ? props.story : props.story.slice(0, 100) + '···'} </Text>
+                <Text style={tw`text-[#191919] text-sm text-justify leading-6`}>{isExpanded ? props.story : props.story.slice(0, 100) + '···'}</Text>
                 <TouchableOpacity onPress={() => { setIsExpanded(!isExpanded); }} style={tw`ml-[-20px]`}>
                     <Image source={ isExpanded ? require('@images/chevron_up.png') : require('@images/chevron_down.png') } style={tw`w-[14.4px] h-[8px]`} />
                 </TouchableOpacity>
