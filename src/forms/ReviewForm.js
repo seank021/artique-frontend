f9a4d33026b752afa838f903a1d41f5646550f80
Sean <seahnnnnnn@gmail.com>
2023-09-06 13:19:01 +0900

    feat: 형식 수정

--- src/forms/ReviewForm.js
+++ src/forms/ReviewForm.js
@@ -1,7 +1,5 @@
-// TODO: 좋아요 누르면 font-medium, 좋아요 안 눌렀으면 font-normal
-
-import React, {useState, useEffect} from 'react';
-import {View, Text, Image, Pressable} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, Pressable } from 'react-native';
 
 import AlertForm from '@forms/AlertForm';
 
@@ -9,110 +7,87 @@ import tw from 'twrnc';
 
 // props: reviewInfo.reviews[i], onPressThumbsUp, onPressArrowCircledRight, isCookie
 export function ShortReviewForm(props) {
-  const [isThumbsUp, setIsThumbsUp] = useState(props.reviewInfo.isThumbsUp);
-  const [isFontMedium, setIsFontMedium] = useState('font-normal');
-  const [thumbsUpImg, setThumbsUpImg] = useState(
-    require('@images/thumbs_up.png'),
-  );
+    const [isThumbsUp, setIsThumbsUp] = useState(props.reviewInfo.isThumbsUp);
+    const [isFontMedium, setIsFontMedium] = useState('font-normal');
+    const [thumbsUpImg, setThumbsUpImg] = useState(require('@images/thumbs_up.png'));
 
-  const [modalVisible, setModalVisible] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
 
-  const [alertImage, setAlertImage] = useState(require('@images/x_red.png'));
-  const [alertText, setAlertText] = useState('로그인이 필요한 서비스입니다.');
+    const [alertImage, setAlertImage] = useState(require('@images/x_red.png'));
+    const [alertText, setAlertText] = useState('로그인이 필요한 서비스입니다.');
 
-  if (props.isCookie) {
-    useEffect(() => {
-      if (isThumbsUp) {
-        setThumbsUpImg(require('@images/thumbs_up_red.png'));
-        setIsFontMedium('font-bold');
-      } else {
-        setThumbsUpImg(require('@images/thumbs_up.png'));
-        setIsFontMedium('font-normal');
-      }
-    }, [isThumbsUp]);
-  }
-
-  const onPressThumbsUp = () => {
-    if (!props.isCookie) {
-      setModalVisible(!modalVisible);
-      setAlertImage(require('@images/x_red.png'));
-      setAlertText('로그인이 필요한 서비스입니다.');
-      setTimeout(() => {
-        setModalVisible(modalVisible);
-      }, 1000);
-      return;
+    if (props.isCookie) {
+        useEffect(() => {
+            if (isThumbsUp) {
+                setThumbsUpImg(require('@images/thumbs_up_red.png'));
+                setIsFontMedium('font-bold');
+            } else {
+                setThumbsUpImg(require('@images/thumbs_up.png'));
+                setIsFontMedium('font-normal');
+            }
+            }, [isThumbsUp]);
     }
-    setIsThumbsUp(!isThumbsUp);
-    props.onPressThumbsUp(props.reviewInfo.reviewId);
-  };
 
-  const onPressArrowCircledRight = () => {
-    props.onPressArrowCircledRight(props.reviewInfo.reviewId);
-  };
+    const onPressThumbsUp = () => {
+        if (!props.isCookie) {
+            setModalVisible(!modalVisible);
+            setAlertImage(require('@images/x_red.png'));
+            setAlertText('로그인이 필요한 서비스입니다.');
+            setTimeout(() => {
+                setModalVisible(modalVisible);
+            }, 1000);
+            return;
+        }
+        setIsThumbsUp(!isThumbsUp);
+        props.onPressThumbsUp(props.reviewInfo.reviewId);
+    };
 
-  return (
-    <>
-      <View>
-        <AlertForm
-          modalVisible={modalVisible}
-          setModalVisible={setModalVisible}
-          borderColor="#F5F8F5"
-          bgColor="#F5F8F5"
-          image={alertImage}
-          textColor="#191919"
-          text={alertText}></AlertForm>
-      </View>
-      <View style={tw`flex flex-col w-[90%] self-center my-[20px]`}>
-        <View style={tw`flex-row justify-between items-center mb-[15px]`}>
-          <View style={tw`flex-row justify-between items-center`}>
-            <Image
-              source={{uri: props.reviewInfo.memberImageUrl}}
-              style={tw`w-[24px] h-[24px] rounded-full mr-[10px]`}></Image>
-            <Text style={tw`text-[#191919] text-[13px] mr-[15px]`}>
-              {props.reviewInfo.memberNickname}
-            </Text>
-            <Text style={tw`text-[#ABABAB] text-xs`}>
-              {props.reviewInfo.viewDate}
-            </Text>
-          </View>
-          <View style={tw`flex-row items-center`}>
-            <Image
-              source={require('@images/star.png')}
-              style={tw`w-[16px] h-[16px] mr-[3px]`}></Image>
-            <Text style={tw`text-[#191919] text-sm`}>
-              {props.reviewInfo.starRating.toFixed(1)}
-            </Text>
-          </View>
-        </View>
-        <View
-          style={tw`flex-row h-[35px] rounded-sm bg-[#F5F5F5] border-2 border-[#F5F5F5] mb-[15px] items-center`}>
-          <Text style={tw`ml-[5px] text-[#191919] font-medium`}>"</Text>
-          <Text style={tw`text-[#191919] font-medium`}>
-            {props.reviewInfo.shortReview}
-          </Text>
-          <Text style={tw`text-[#191919] font-medium`}>"</Text>
-        </View>
-        <View style={tw`flex-row justify-between items-center`}>
-          <View style={tw`flex-row justify-between items-center`}>
-            <Pressable onPress={onPressThumbsUp}>
-              <Image
-                source={thumbsUpImg}
-                style={tw`w-[20px] h-[20px] mr-[5px]`}></Image>
-            </Pressable>
-            <Text style={tw`text-sm text-[#191919] ${isFontMedium}`}>
-              {props.reviewInfo.thumbsCount}
-            </Text>
-          </View>
-          {/* <Pressable onPress={onPressArrowCircledRight}>
-            <Image
-              source={require('@images/arrow_circled_right.png')}
-              style={tw`w-[20px] h-[20px]`}></Image>
-          </Pressable> */}
-        </View>
-      </View>
-      <View style={tw`border-4 border-[#F5F5F5]`}></View>
-    </>
-  );
+    const onPressArrowCircledRight = () => {
+        props.onPressArrowCircledRight(props.reviewInfo.reviewId);
+    };
+
+    return (
+        <>
+            <View>
+                <AlertForm modalVisible={modalVisible} setModalVisible={setModalVisible} borderColor="#F5F8F5" bgColor="#F5F8F5" image={alertImage} textColor="#191919" text={alertText}></AlertForm>
+            </View>
+            <View style={tw`flex flex-col w-[90%] self-center my-[20px]`}>
+                <View style={tw`flex-row justify-between items-center mb-[15px]`}>
+                    <View style={tw`flex-row justify-between items-center`}>
+                        <Image source={{uri: props.reviewInfo.memberImageUrl}} style={tw`w-[24px] h-[24px] rounded-full mr-[10px]`}></Image>
+                        <Text style={tw`text-[#191919] text-[13px] mr-[15px]`}>{props.reviewInfo.memberNickname}</Text>
+                        <Text style={tw`text-[#ABABAB] text-xs`}>{props.reviewInfo.viewDate}</Text>
+                    </View>
+                    <View style={tw`flex-row items-center`}>
+                        <Image source={require('@images/star.png')} style={tw`w-[16px] h-[16px] mr-[3px]`}></Image>
+                        <Text style={tw`text-[#191919] text-sm`}>{props.reviewInfo.starRating.toFixed(1)}</Text>
+                    </View>
+                </View>
+                <View style={tw`flex-row h-[35px] rounded-sm bg-[#F5F5F5] border-2 border-[#F5F5F5] mb-[15px] items-center`}>
+                    <Text style={tw`ml-[5px] text-[#191919] font-medium`}>"</Text>
+                    <Text style={tw`text-[#191919] font-medium`}>{props.reviewInfo.shortReview}</Text>
+                    <Text style={tw`text-[#191919] font-medium`}>"</Text>
+                </View>
+                <View style={tw`flex-row justify-between items-center`}>
+                <View style={tw`flex-row justify-between items-center`}>
+                    <Pressable onPress={onPressThumbsUp}>
+                        <Image source={thumbsUpImg} style={tw`w-[20px] h-[20px] mr-[5px]`}></Image>
+                    </Pressable>
+                    <Text style={tw`text-sm text-[#191919] ${isFontMedium}`}>{props.reviewInfo.thumbsCount}</Text>
+                </View>
+                {/* <Pressable onPress={onPressArrowCircledRight}>
+                    <Image
+                    source={require('@images/arrow_circled_right.png')}
+                    style={tw`w-[20px] h-[20px]`}></Image>
+                </Pressable> */}
+                </View>
+            </View>
+            <View style={tw`border-4 border-[#F5F5F5]`}></View>
+        </>
+    );
 }
 
-export function ShortReviewFormInFeed(props) {}
+export function ShortReviewFormInFeed(props) {
+
+}
+
--- src/screens/Main/MusicalDetail1.js
+++ src/screens/Main/MusicalDetail1.js
@@ -1,16 +1,8 @@
 // 주의사항: isCookie 여부에 따라 유저 권한 다르게 주기
 
-import React, {useState} from 'react';
-import {
-  View,
-  Pressable,
-  Text,
-  ScrollView,
-  Image,
-  Alert,
-  StyleSheet,
-} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import React, { useState } from 'react';
+import { View, Pressable, Text, ScrollView, Image, Alert, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import tw from 'twrnc';
 
@@ -19,135 +11,107 @@ import AlertForm from '@forms/AlertForm';
 import MusicalInfoForm from '@forms/MusicalInfoForm';
 import StoryForm from '@forms/StoryForm';
 import AverageScoreForm from '@forms/AverageScoreForm';
-import {ShortReviewForm} from '@forms/ReviewForm';
+import { ShortReviewForm } from '@forms/ReviewForm';
 
-import {useNavigation} from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 
-import {musicalInfo} from '@functions/api';
-import {scoreCount} from '@functions/api';
-import {reviewInfo} from '@functions/api';
+import { musicalInfo } from '@functions/api';
+import { scoreCount } from '@functions/api';
+import { reviewInfo } from '@functions/api';
 
 export default function MusicalDetail1({isCookie}) {
-  const [modalVisible, setModalVisible] = useState(false);
+    const [modalVisible, setModalVisible] = useState(false);
 
-  const [alertImage, setAlertImage] = useState(require('@images/x_red.png'));
-  const [alertText, setAlertText] = useState('로그인이 필요한 서비스입니다.');
+    const [alertImage, setAlertImage] = useState(require('@images/x_red.png'));
+    const [alertText, setAlertText] = useState('로그인이 필요한 서비스입니다.');
 
-  const nav = useNavigation();
+    const nav = useNavigation();
 
-  const goBack = () => {
-    nav.goBack();
-  };
+    const goBack = () => {
+        nav.goBack();
+    };
 
-  const onPressWrite = () => {
-    if (!isCookie) {
-      setModalVisible(!modalVisible);
-      setAlertImage(require('@images/x_red.png'));
-      setAlertText('로그인이 필요한 서비스입니다.');
-      setTimeout(() => {
-        setModalVisible(modalVisible);
-      }, 1000);
-      return;
-    }
-    console.log(musicalInfo.musicalId);
-    Alert.alert('리뷰 작성 페이지로 이동');
-  };
+    const onPressWrite = () => {
+        if (!isCookie) {
+        setModalVisible(!modalVisible);
+        setAlertImage(require('@images/x_red.png'));
+        setAlertText('로그인이 필요한 서비스입니다.');
+        setTimeout(() => {
+            setModalVisible(modalVisible);
+        }, 1000);
+        return;
+        }
+        console.log(musicalInfo.musicalId);
+        Alert.alert('리뷰 작성 페이지로 이동');
+    };
 
-  const goToReviewDetail1 = () => {
-    nav.navigate('ReviewDetail1');
-  };
+    const goToReviewDetail1 = () => {
+        nav.navigate('ReviewDetail1');
+    };
 
-  const onPressThumbsUp = reviewId => {
-    console.log(reviewId);
-  };
+    const onPressThumbsUp = reviewId => {
+        console.log(reviewId);
+    };
 
-  const goToSeeMore1 = reviewId => {
-    console.log(reviewId);
-    nav.navigate('SeeMore1');
-  };
+    const goToSeeMore1 = reviewId => {
+        console.log(reviewId);
+        nav.navigate('SeeMore1');
+    };
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <AlertForm
-          modalVisible={modalVisible}
-          setModalVisible={setModalVisible}
-          borderColor="#F5F8F5"
-          bgColor="#F5F8F5"
-          image={alertImage}
-          textColor="#191919"
-          text={alertText}></AlertForm>
-      </View>
-      <View style={tw`flex-row items-center justify-between mt-5 mb-[14px]`}>
-        <Pressable onPress={goBack}>
-          <Image
-            source={require('@images/chevron_left.png')}
-            style={tw`ml-[20px] w-[10px] h-[18px] tint-[#191919]`}></Image>
-        </Pressable>
-        <Text style={tw`ml-[8px] text-[#191919] text-base font-medium`}>
-          {musicalInfo.title}
-        </Text>
-        <Pressable onPress={onPressWrite}>
-          <Image
-            source={require('@images/write.png')}
-            style={tw`mr-[20px] w-[18px] h-[17.121px] tint-[#191919]`}></Image>
-        </Pressable>
-      </View>
-      <View style={tw`border-solid border-b border-[#D3D4D3]`}></View>
+    return (
+        <SafeAreaView style={styles.container}>
+            <View>
+                <AlertForm
+                modalVisible={modalVisible}
+                setModalVisible={setModalVisible}
+                borderColor="#F5F8F5"
+                bgColor="#F5F8F5"
+                image={alertImage}
+                textColor="#191919"
+                text={alertText}></AlertForm>
+            </View>
+            <View style={tw`flex-row items-center justify-between mt-5 mb-[14px]`}>
+                <Pressable onPress={goBack}>
+                <Image
+                    source={require('@images/chevron_left.png')}
+                    style={tw`ml-[20px] mr-[8px] w-[10px] h-[18px] tint-[#191919]`}></Image>
+                </Pressable>
+                <Text style={tw`text-[#191919] text-base font-medium`}>
+                {musicalInfo.title}
+                </Text>
+                <Pressable onPress={onPressWrite}>
+                <Image
+                    source={require('@images/write.png')}
+                    style={tw`mr-[20px] w-[18px] h-[17.121px] tint-[#191919]`}></Image>
+                </Pressable>
+            </View>
+            <View style={tw`border-solid border-b border-[#D3D4D3]`}></View>
 
-      <ScrollView>
-        <View style={tw`mt-[27.56px]`}></View>
-        <MusicalInfoForm
-          poster={musicalInfo.poster}
-          title={musicalInfo.title}
-          score={musicalInfo.averageScore}
-          date={musicalInfo.date}
-          place={musicalInfo.place}
-          duration={musicalInfo.duration}
-          casting={musicalInfo.casting}></MusicalInfoForm>
-        <View style={tw`mb-[27.56px]`}></View>
-        <StoryForm story={musicalInfo.story}></StoryForm>
-        <View style={tw`mb-[35px]`}></View>
-        <AverageScoreForm
-          averageScore={musicalInfo.averageScore}
-          scoreCount={scoreCount}></AverageScoreForm>
-        <View style={tw`mb-[35px]`}></View>
-        <View
-          style={tw`flex flex-row w-[90%] justify-between items-center self-center mb-[10px]`}>
-          <Text style={tw`text-[#191919] text-base font-medium`}>
-            리뷰 ({reviewInfo.totalReviewCount})
-          </Text>
-          <Pressable onPress={goToReviewDetail1}>
-            <Text style={tw`text-xs text-[#191919]`}>전체보기</Text>
-          </Pressable>
-        </View>
-        <ShortReviewForm
-          reviewInfo={reviewInfo.reviews[0]}
-          onPressThumbsUp={() =>
-            onPressThumbsUp(reviewInfo.reviews[0].reviewId)
-          }
-          onPressArrowCircledRight={() =>
-            goToSeeMore1(reviewInfo.reviews[0].reviewId)
-          }
-          isCookie={isCookie}></ShortReviewForm>
-        <ShortReviewForm
-          reviewInfo={reviewInfo.reviews[1]}
-          onPressThumbsUp={() =>
-            onPressThumbsUp(reviewInfo.reviews[1].reviewId)
-          }
-          onPressArrowCircledRight={() =>
-            goToSeeMore1(reviewInfo.reviews[1].reviewId)
-          }
-          isCookie={isCookie}></ShortReviewForm>
-      </ScrollView>
-    </SafeAreaView>
-  );
+            <ScrollView>
+                <View style={tw`mt-[27.56px]`}></View>
+                <MusicalInfoForm poster={musicalInfo.poster} title={musicalInfo.title} score={musicalInfo.averageScore} date={musicalInfo.date} place={musicalInfo.place} duration={musicalInfo.duration} casting={musicalInfo.casting}></MusicalInfoForm>
+                <View style={tw`mb-[27.56px]`}></View>
+                <StoryForm story={musicalInfo.story}></StoryForm>
+                <View style={tw`mb-[35px]`}></View>
+                <AverageScoreForm averageScore={musicalInfo.averageScore} scoreCount={scoreCount}></AverageScoreForm>
+                <View style={tw`mb-[35px]`}></View>
+                <View style={tw`flex flex-row w-[90%] justify-between items-center self-center mb-[10px]`}>
+                    <Text style={tw`text-[#191919] text-base font-medium`}>리뷰 ({reviewInfo.totalReviewCount})</Text>
+                    <Pressable onPress={goToReviewDetail1}>
+                        <Text style={tw`text-xs text-[#191919]`}>전체보기</Text>
+                    </Pressable>
+                </View>
+                <ShortReviewForm reviewInfo={reviewInfo.reviews[0]} onPressThumbsUp={() => onPressThumbsUp(reviewInfo.reviews[0].reviewId) } onPressArrowCircledRight={() => goToSeeMore1(reviewInfo.reviews[0].reviewId) } isCookie={isCookie}></ShortReviewForm>
+                <ShortReviewForm reviewInfo={reviewInfo.reviews[1]} onPressThumbsUp={() => onPressThumbsUp(reviewInfo.reviews[1].reviewId) } onPressArrowCircledRight={() => goToSeeMore1(reviewInfo.reviews[1].reviewId) } isCookie={isCookie}></ShortReviewForm>
+            </ScrollView>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#FAFAFA',
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#FAFAFA',
+    },
 });
