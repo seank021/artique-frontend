e3e0e38391b1d4fb0db2f96f3e39a28cef7f4523
Sean <seahnnnnnn@gmail.com>
2023-09-09 23:01:28 +0900

    fix: TouchableOpacity -> Pressable로 수정

--- src/forms/StoryForm.js
+++ src/forms/StoryForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, Pressable } from 'react-native';
 import tw from 'twrnc';
 
 // props: story
@@ -11,9 +11,9 @@ export default function StoryForm(props) {
             <Text style={tw`text-[#191919] text-base font-medium mb-[10px]`}>줄거리</Text>
             <View style={{ flexDirection: 'row', alignItems: 'baseline', }}>
                 <Text style={tw`text-[#191919] text-sm text-justify leading-6`}>{isExpanded ? props.story : props.story.slice(0, 100) + '···'}</Text>
-                <TouchableOpacity onPress={() => { setIsExpanded(!isExpanded); }} style={tw`ml-[-20px]`}>
+                <Pressable onPress={() => { setIsExpanded(!isExpanded); }} style={tw`ml-[-20px]`}>
                     <Image source={ isExpanded ? require('@images/chevron_up.png') : require('@images/chevron_down.png') } style={tw`w-[14.4px] h-[8px]`} />
-                </TouchableOpacity>
+                </Pressable>
             </View>
         </View>
     );
