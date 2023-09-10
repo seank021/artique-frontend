ee6925e7ab0790bb9db22e39649fd2ddd535a0d8
Sean <seahnnnnnn@gmail.com>
2023-09-10 22:40:31 +0900

    fix: 에러메시지 출력 형식 변경

--- src/screens/LoginSignup/Login1.js
+++ src/screens/LoginSignup/Login1.js
@@ -80,7 +80,7 @@ export default function Login1({setGoToFeed}) {
         console.log(err);
       }
     } catch (err) {
-      console.log(err);
+      console.log(err.response.data);
     }
   };
 
