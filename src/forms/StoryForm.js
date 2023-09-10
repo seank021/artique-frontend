ead64e0bb62131c0a46199c7f907b0fc183d37f5
Sean <seahnnnnnn@gmail.com>
2023-09-10 22:40:09 +0900

    feat: 오류 방지 로직 구현

--- src/forms/StoryForm.js
+++ src/forms/StoryForm.js
@@ -9,12 +9,23 @@ export default function StoryForm(props) {
     return (
         <View style={tw`flex flex-col w-[90%] self-center`}>
             <Text style={tw`text-[#191919] text-base font-medium mb-[10px]`}>줄거리</Text>
-            <View style={{ flexDirection: 'row', alignItems: 'baseline', }}>
-                <Text style={tw`text-[#191919] text-sm text-justify leading-6`}>{isExpanded ? props.story : props.story.slice(0, 100) + '···'}</Text>
-                <Pressable onPress={() => { setIsExpanded(!isExpanded); }} style={tw`ml-[-20px]`}>
-                    <Image source={ isExpanded ? require('@images/chevron_up.png') : require('@images/chevron_down.png') } style={tw`w-[14.4px] h-[8px]`} />
-                </Pressable>
-            </View>
+            {props.story ? (
+                <View style={{ flexDirection: 'row', alignItems: 'baseline' }}>
+                    <Text style={tw`text-[#191919] text-sm text-justify leading-6`}>
+                        {isExpanded ? props.story : props.story.slice(0, 100) + '···'}
+                    </Text>
+                    <Pressable onPress={() => { setIsExpanded(!isExpanded); }} style={tw`ml-[-20px]`}>
+                        <Image
+                            source={isExpanded ? require('@images/chevron_up.png') : require('@images/chevron_down.png')}
+                            style={tw`w-[14.4px] h-[8px]`}
+                        />
+                    </Pressable>
+                </View>
+            ) : (
+                <Text style={tw`text-[#191919] text-sm text-justify leading-6`}>
+                    줄거리 정보 없음
+                </Text>
+            )}
         </View>
     );
 }
