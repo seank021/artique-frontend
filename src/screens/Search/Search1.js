fab51d819849733bee6411ab983d9893108b7d23
Sean <seahnnnnnn@gmail.com>
2023-10-02 00:27:12 +0900

    feat: 검색 후 상황 구현

--- src/screens/Search/Search1.js
+++ src/screens/Search/Search1.js
@@ -8,8 +8,11 @@ import tw from 'twrnc'
 
 import * as Keywords from "@functions/keywords";
 
-export default function Search1({isCookie}) {    
+export default function Search1({isCookie}) {
+    const [isBeforeSearch, setIsBeforeSearch] = useState(true); // 검색 전, 후 구분
+
     const [value, setValue] = useState('');
+    const [placeholderValue, setPlaceholderValue] = useState('');
     const [ifX, setIfX] = useState(false);
     
     const [searchHistory, setSearchHistory] = useState([]);
@@ -34,17 +37,24 @@ export default function Search1({isCookie}) {
         setValue('');
         this.textInput.clear();
         setIfX(false);
+        setIsBeforeSearch(true);
     }
 
-    const onPressSearch = () => {
-        if (value === '') {
+    const onPressSearch = (keyword) => {
+        if (keyword === '') {
             return;
         }
-        Keywords.storeSearchKeyword(value);
-        if (searchHistory.includes(value)) {
+
+        console.log(keyword);
+
+        Keywords.storeSearchKeyword(keyword);
+        setIsBeforeSearch(false);
+        setPlaceholderValue(keyword);
+        setValue('');
+        if (searchHistory.includes(keyword)) {
             return;
         }
-        setSearchHistory([...searchHistory, value]);
+        setSearchHistory([keyword, ...searchHistory]);
     }
 
     const deleteKeyword = (keyword) => {
@@ -63,42 +73,60 @@ export default function Search1({isCookie}) {
 
     return (
         <SafeAreaView style={styles.container}>
-            <View style={tw`mx-[5%] mt-[17px] mb-[11px]`}>
-                <View style={tw`flex-row justify-between bg-[#E6E6E6] rounded-[19.5px]`}>
-                    <View style={tw`flex-row items-center`}>
-                        <Image source={require('@images/search.png')} style={tw`ml-[18px] w-[18px] h-[18px] tint-[#ABABAB]`} />
-                        <TextInput ref={(text) => this.textInput = text} onChangeText={onChangeText} style={tw`ml-[14px]`} placeholder='작품명이나 배우를 검색해보세요' />
-                    </View>
-                    <View style={tw`self-center`}>
-                        {ifX ? <Pressable onPress={deleteTextInput}><Image source={require('@images/x.png')} style={tw`mr-[18px] w-[18px] h-[18px] tint-[#ABABAB]`} /></Pressable> : null}
-                    </View>
-                </View>
-            </View>
-            <View style={tw`border-[0.5px] border-[#D3D4D3]`}></View>
-
-            <View style={tw`flex-row mx-[5%] mt-[16px]`}>
-                {searchHistory.length !== 0 ? 
-                    <>
-                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                            {searchHistory.map((keyword, index) => {
-                                return (
-                                    <View key={index} style={tw`flex-row items-center`}>
-                                        <Text style={tw`text-[#ABABAB] text-sm`}>{keyword}</Text>
-                                        <Pressable onPress={() => deleteKeyword(keyword)}><Image source={require('@images/x.png')} style={tw`ml-[5px] mr-[13px] w-[14px] h-[14px] tint-[#ABABAB]`} /></Pressable>
-                                    </View>
-                                );
-                            })}
-                        </ScrollView>
-                        <View>
-                            <Pressable onPress={deleteAllKeywords}><Text style={tw`text-[#191919] text-sm ml-[8px]`}>전체 삭제</Text></Pressable>
+            {isBeforeSearch ?
+                <>
+                    <View style={tw`mx-[5%] mt-[17px] mb-[11px]`}>
+                        <View style={tw`flex-row justify-between bg-[#E6E6E6] rounded-[19.5px]`}>
+                            <View style={tw`flex-row w-[90%] items-center`}>
+                                <Image source={require('@images/search.png')} style={tw`ml-[18px] w-[18px] h-[18px] tint-[#ABABAB]`} />
+                                <TextInput ref={(text) => this.textInput = text} onChangeText={onChangeText} style={tw`ml-[14px]`} placeholder='작품명이나 배우를 검색해보세요' />
+                            </View>
+                            <View style={tw`self-center`}>
+                                {ifX ? <Pressable onPress={deleteTextInput}><Image source={require('@images/x.png')} style={tw`mr-[18px] w-[18px] h-[18px] tint-[#ABABAB]`} /></Pressable> : null}
+                            </View>
                         </View>
-                    </>
-                    : <Text style={tw`text-[#ABABAB] text-sm`}>최근 검색어가 없습니다.</Text>
-                }
-            </View>
+                    </View>
+                    <View style={tw`border-[0.5px] border-[#D3D4D3]`}></View>
 
-            <View style={tw`mt-[100px]`}></View>
-            <Button onPress={onPressSearch} title="임의로 만든 검색 버튼" />
+                    <View style={tw`flex-row mx-[5%] mt-[16px]`}>
+                        {searchHistory.length !== 0 ? 
+                            <>
+                                <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                                    {searchHistory.map((keyword, index) => {
+                                        return (
+                                            <View key={index} style={tw`flex-row items-center`}>
+                                                <Pressable onPress={() => onPressSearch(keyword)}><Text style={tw`text-[#ABABAB] text-sm`}>{keyword}</Text></Pressable>
+                                                <Pressable onPress={() => deleteKeyword(keyword)}><Image source={require('@images/x.png')} style={tw`ml-[5px] mr-[13px] w-[14px] h-[14px] tint-[#ABABAB]`} /></Pressable>
+                                            </View>
+                                        );
+                                    })}
+                                </ScrollView>
+                                <View>
+                                    <Pressable onPress={deleteAllKeywords}><Text style={tw`text-[#191919] text-sm ml-[8px]`}>전체 삭제</Text></Pressable>
+                                </View>
+                            </>
+                            : <Text style={tw`text-[#ABABAB] text-sm`}>최근 검색어가 없습니다.</Text>
+                        }
+                    </View>
+
+                    <View style={tw`mt-[100px]`}></View>
+                    <Button onPress={() => onPressSearch(value)} title="임의로 만든 검색 버튼" />
+                </>
+            :
+                <>
+                    <View style={tw`flex-row items-center mx-[5%] mt-[17px] mb-[11px]`}>
+                        <Pressable onPress={()=>setIsBeforeSearch(true)}><Image source={require('@images/chevron_left.png')} style={tw`w-[10px] h-[18px] mr-[23px] tint-[#191919]`} /></Pressable>
+                        <View style={tw`flex-row w-[90%] justify-between items-center bg-[#E6E6E6] rounded-[19.5px]`}>
+                            <TextInput style={tw`ml-[14px] font-medium`} placeholder={placeholderValue} placeholderTextColor={"#191919"} editable={false} />
+                            <Pressable onPress={()=>setIsBeforeSearch(true)}><Image source={require('@images/x.png')} style={tw`mr-[18px] w-[18px] h-[18px] tint-[#ABABAB]`} /></Pressable>
+                        </View>
+                    </View>
+                    <View style={tw`border-[0.5px] border-[#D3D4D3]`}></View>
+
+                    <Text>POSTERS</Text>
+                    
+                </>
+            }
 
 
         </SafeAreaView>
