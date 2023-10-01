107db53f580215bed2f5e1c631b831daffa126fd
Sean <seahnnnnnn@gmail.com>
2023-10-01 22:17:34 +0900

    feat: 검색어 입력 시 x 버튼 나타남, 누르면 검색어 삭제

--- src/screens/Search/Search.js
+++ src/screens/Search/Search.js
@@ -4,18 +4,43 @@
 // TODO: 검색어 입력 시 x 버튼 누르면 검색어 삭제
 // TODO: 나머지 스크린 구현
 
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet, Image, TextInput, ScrollView, Text, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from 'twrnc'
 
 export default function Search() {
+    const [value, setValue] = useState('');
+    const [ifX, setIfX] = useState(false);
+
+    const onChangeText = (text) => {
+        setValue(text);
+        if (text === '') {
+            setIfX(false);
+        } else {
+            setIfX(true);
+        }
+    }
+
+    const deleteTextInput = () => {
+        setValue('');
+        this.textInput.clear();
+        setIfX(false);
+    }
+
+    console.log(value);
+
     return (
         <SafeAreaView style={styles.container}>
-            <View style={tw`mx-[5%] mt-[18px] mb-[12px] flex-col`}>
-                <View style={tw`flex-row items-center justify-between bg-[#E6E6E6] rounded-[19.5px] h-[37.64781px]`}>
-                    <Image source={require('@images/search.png')} style={tw`ml-[18px] w-[18px] h-[18px] tint-[#ABABAB]`} />
-                    <TextInput style={tw`w-[90%] ml-[14px]`} placeholder='작품명이나 배우를 검색해보세요' />
+            <View style={tw`mx-[5%] mt-[17px] mb-[11px] flex-col`}>
+                <View style={tw`flex-row justify-between bg-[#E6E6E6] rounded-[19.5px]`}>
+                    <View style={tw`flex-row items-center`}>
+                        <Image source={require('@images/search.png')} style={tw`ml-[18px] w-[18px] h-[18px] tint-[#ABABAB]`} />
+                        <TextInput ref={(text) => this.textInput = text} onChangeText={onChangeText} style={tw`ml-[14px]`} placeholder='작품명이나 배우를 검색해보세요' />
+                    </View>
+                    <View style={tw`self-center`}>
+                        {ifX ? <Pressable onPress={deleteTextInput}><Image source={require('@images/x.png')} style={tw`mr-[18px] w-[18px] h-[18px] tint-[#ABABAB]`} /></Pressable> : null}
+                    </View>
                 </View>
             </View>
             <View style={tw`border-[0.5px] border-[#D3D4D3]`}></View>
