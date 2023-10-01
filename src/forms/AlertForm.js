41cc03007c2c5249bf3cfabbeed42d6c77b3c834
eojin03 <elena1014@naver.com>
2023-10-01 12:50:15 +0900

    feat: reviewdetail page 반응형으로 바꿔보기

--- src/forms/AlertForm.js
+++ src/forms/AlertForm.js
@@ -68,10 +68,10 @@ export function AlertFormForSort(props) {
 export function LongReviewModal(props) {
     return (
         <Modal animationIn={"fadeIn"} animationOut={"fadeOut"} transparent={true} isVisible={props.longReviewModalVisible} hasBackdrop={true} backdropOpacity={0.5} onBackdropPress={() => props.setLongReviewModalVisible(false)}>
-            <View style={tw`flex flex-col h-[600px] bg-white rounded-[15px] self-center items-center justify-between`}>
-                <Text style={tw`text-base text-[#191919] font-medium mt-[24px]`}>긴줄평</Text>
-                <ScrollView style={tw`mx-[10%] mb-[57px]`} showsVerticalScrollIndicator={false}>
-                    <Text style={tw`text-sm font-normal text-justify mt-[31px] text-[#191919] leading-[23px]`}>{props.longReview}</Text>
+            <View style={tw`flex flex-col w-9.5/10 h-7/10 bg-white rounded-2xl self-center items-center justify-between`}>
+                <Text style={tw`text-base text-[#191919] font-medium mt-6`}>긴줄평</Text>
+                <ScrollView style={tw`mx-8 mb-14`} showsVerticalScrollIndicator={false}>
+                    <Text style={tw`text-sm font-normal text-justify mt-8 text-[#191919] leading-6`}>{props.longReview}</Text>
                 </ScrollView>
             </View>
         </Modal>
