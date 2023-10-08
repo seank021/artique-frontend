d855a9827a93e8b65bc64251a3a9f81f1dae7085
Sean <seahnnnnnn@gmail.com>
2023-10-08 23:25:19 +0900

    feat: 검색 구현 로직 수정

--- src/screens/Search/Search1.js
+++ src/screens/Search/Search1.js
@@ -85,6 +85,7 @@ export default function Search1({ isCookie, setMusicalId }) {
             setSearchHistory([keyword, ...searchHistory.filter((item) => item !== keyword)]);
         }
 
+        setSortCriteria('최신순');
         setSearchValue(keyword);
     }
 
@@ -100,6 +101,11 @@ export default function Search1({ isCookie, setMusicalId }) {
     const deleteAllKeywords = () => {
         Keywords.removeAllKeywords();
         setSearchHistory([]);
+    }
+
+    const searchAgain = () => {
+        setSearchValue('');
+        setValue('');
     }
 
     const onPressMusical = (musicalId) => {
@@ -140,7 +146,7 @@ export default function Search1({ isCookie, setMusicalId }) {
                                 <TextInput ref={(text) => this.textInput = text} onChangeText={(text) => onChangeText(text)} onSubmitEditing={() => onPressSearch(value)} returnKeyType="done" placeholder='작품명이나 배우를 검색해보세요' style={tw`ml-[14px]`}  />
                             </View>
                             <View style={tw`self-center`}>
-                                {ifX ? <Pressable onPress={deleteTextInput}><Image source={require('@images/x.png')} style={tw`mr-[18px] w-[18px] h-[18px] tint-[#ABABAB]`} /></Pressable> : null}
+                                {ifX ? <Pressable onPress={deleteTextInput}><Image source={require('@images/x.png')} style={tw`mr-[12px] w-[16px] h-[16px] tint-[#ABABAB]`} /></Pressable> : null}
                             </View>
                         </View>
                     </View>
@@ -170,25 +176,32 @@ export default function Search1({ isCookie, setMusicalId }) {
             :
                 <>
                     <View style={tw`flex-row items-center mx-[5%] mt-[17px] mb-[11px]`}>
-                        <Pressable onPress={()=> {setIsBeforeSearch(true); setSortCriteria('최신순');}}><Image source={require('@images/chevron_left.png')} style={tw`w-[10px] h-[18px] mr-[23px] tint-[#191919]`} /></Pressable>
-                        <View style={tw`flex-row w-[90%] h-[100%] justify-between items-center bg-[#E6E6E6] min-h-[34px] rounded-[19.5px]`}>
-                            <TextInput style={tw`ml-[14px] font-medium`} placeholder={placeholderValue} placeholderTextColor={"#191919"} editable={false} />
-                            <Pressable onPress={()=> {setIsBeforeSearch(true); setSortCriteria('최신순');}}><Image source={require('@images/x.png')} style={tw`mr-[18px] w-[18px] h-[18px] tint-[#ABABAB]`} /></Pressable>
+                        <Pressable onPress={()=> {setIsBeforeSearch(true); setSortCriteria('최신순'); setIfX(false);}}><Image source={require('@images/chevron_left.png')} style={tw`w-[10px] h-[18px] mr-[22.5px] tint-[#191919]`} /></Pressable>
+                        <View style={tw`flex-row w-[90%] h-[100%] justify-between bg-[#E6E6E6] min-h-[34px] rounded-[19.5px]`}>
+                            <View style={tw`flex-row items-center`}>
+                                <TextInput style={tw`mx-[14px] font-medium`} defaultValue={placeholderValue} width="80%" onFocus={searchAgain} ref={(text) => this.textInput = text} onChangeText={(text) => onChangeText(text)} onSubmitEditing={() => onPressSearch(value)} returnKeyType="done" />
+                            </View>
+                            <View style={tw`self-center`}>
+                                <Pressable onPress={()=> {setIsBeforeSearch(true); setSortCriteria('최신순');}}><Image source={require('@images/x.png')} style={tw`mr-[12px] w-[16px] h-[16px] tint-[#ABABAB]`} /></Pressable>
+                            </View>
                         </View>
                     </View>
                     <View style={tw`border-[0.5px] border-[#D3D4D3]`}></View>
 
-                    <Pressable style={tw`flex flex-row items-center justify-end mr-[5%] my-[15px]`} onPress={() => setSortModalVisible(true)}>
-                        <Text style={tw`text-[#191919] text-xs font-medium mr-[7px]`}>{sortCriteria}</Text>
-                        <Image source={require('@images/chevron_down.png')} style={tw`w-[14.4px] h-[8px]`}></Image>
-                    </Pressable>
-                    <AlertFormForSort2 sortModalVisible={sortModalVisible} setSortModalVisible={setSortModalVisible} sortCriteria={sortCriteria} setSortCriteria={setSortCriteria}></AlertFormForSort2>
-                    
                     {searchedMusicals.length !== 0 ?
-                        <View style={{flex : 1, marginHorizontal : "2%"}}>
-                            <MusicalsList data={searchedMusicals} />
-                        </View>
-                    : <Text style={tw`text-[#ABABAB] text-sm mx-[5%]`}>검색 결과가 없습니다.</Text>}
+                        <>
+                            <Pressable style={tw`flex flex-row items-center justify-end mr-[5%] my-[15px]`} onPress={() => setSortModalVisible(true)}>
+                                <Text style={tw`text-[#191919] text-xs font-medium mr-[7px]`}>{sortCriteria}</Text>
+                                <Image source={require('@images/chevron_down.png')} style={tw`w-[14.4px] h-[8px]`}></Image>
+                            </Pressable>
+                            <AlertFormForSort2 sortModalVisible={sortModalVisible} setSortModalVisible={setSortModalVisible} sortCriteria={sortCriteria} setSortCriteria={setSortCriteria}></AlertFormForSort2>
+                        
+                            <View style={{flex : 1, marginHorizontal : "2%"}}>
+                                <MusicalsList data={searchedMusicals} />
+                            </View>
+                        </>
+                        : <Text style={tw`text-[#ABABAB] text-sm mx-[5%] my-[15px]`}>검색 결과가 없습니다.</Text>
+                    }
                 </>
             }
         </SafeAreaView>
