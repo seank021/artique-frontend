01072c713409c6f57d6cbab1045ccf539491e8dd
Sean <seahnnnnnn@gmail.com>
2023-11-04 23:40:55 +0900

    feat: 리뷰 작성 페이지 스크린 구현 완료

---
+++ src/forms/ReviewContentsForm.js
@@ -0,0 +1,181 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, Pressable, Image, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Modal from 'react-native-modal';
+
+import tw from 'twrnc';
+
+// 캐스팅 입력 모듈
+// props: modalVisible, setModalVisible, castings, setFinalCastings
+export const CastingForm = (props) => {
+    const [casting, setCasting] = useState("") // 배우 이름
+    const [castings, setCastings] = useState(props.castings) // 배우 이름 리스트
+
+    const addCasting = () => {
+        if (casting.trim() === "") return;
+        setCastings([...castings, casting]);
+        setCasting("");
+    }
+
+    const removeCasting = (index) => {
+        setCastings(castings.filter((cast, i) => i !== index));
+    }
+
+    const onPressSave = () => { // castings ReviewWrite에 넘겨주기
+        props.setFinalCastings(castings);
+        props.setModalVisible(false);
+    }
+
+    return (
+        <Modal animationIn={"fadeIn"} animationOut={"fadeOut"} transparent={true} isVisible={props.modalVisible} hasBackdrop={true} backdropOpacity={0.5} onBackdropPress={() => props.setModalVisible(false)}>
+            <View style={tw`flex flex-col justify-around items-center self-center w-[230px] bg-[#FAFAFA] rounded-[15px]`}>
+                <Text style={tw`text-[#191919] text-base font-medium mt-[24px] mb-[12px]`}>캐스팅 입력하기</Text>
+                {castings.map((cast, index) => (
+                    <View key={index} style={tw`flex-row items-center`}>
+                        <Text style={tw`text-[#191919] text-sm my-[7.5px] mr-[10px]`}>{cast}</Text>
+                        <Pressable onPress={() => removeCasting(index)}><Image source={require('@images/x.png')} style={tw`w-[14px] h-[14px] tint-[#ABABAB]`}></Image></Pressable>
+                    </View>
+                ))}
+                <TextInput style={tw`text-sm mb-[15px]`} placeholder="배우 이름을 입력해주세요" placeholderTextColor="#B6B6B6" onChangeText={(text) => setCasting(text)} value={casting}></TextInput>
+                <Pressable onPress={addCasting}><Image source={require('@images/add_button.png')} style={tw`w-[20px] h-[20px] mb-[30px] mt-[5px]`}></Image></Pressable>
+                <View style={tw`w-[100%] border-[0.5px] border-[#D3D4D3]`}></View>
+                <Pressable onPress={onPressSave}><Text style={tw`text-[#191919] text-sm my-[15px]`}>저장</Text></Pressable>
+            </View>
+        </Modal>
+    )
+}
+
+// 좌석 입력 모듈
+// props: modalVisible, setModalVisible, seat, setFinalSeat
+export const SeatForm = (props) => {
+    const [seat, setSeat] = useState(props.seat) // 좌석 위치
+
+    const onPressSave = () => { // seat ReviewWrite에 넘겨주기
+        if (seat.trim() === "") return;
+        setSeat(seat);
+        props.setFinalSeat(seat);
+        props.setModalVisible(false);
+    }
+
+    return (
+        <Modal animationIn={"fadeIn"} animationOut={"fadeOut"} transparent={true} isVisible={props.modalVisible} hasBackdrop={true} backdropOpacity={0.5} onBackdropPress={() => props.setModalVisible(false)}>
+            <View style={tw`flex flex-col justify-around items-center self-center w-[230px] bg-[#FAFAFA] rounded-[15px]`}>
+                <Text style={tw`text-[#191919] text-base font-medium mt-[24px] mb-[12px]`}>좌석 입력하기</Text>
+                <TextInput style={tw`text-sm mb-[15px]`} defaultValue={seat} placeholder="좌석 위치를 입력해주세요" placeholderTextColor="#B6B6B6" onChangeText={(text) => setSeat(text)}></TextInput>
+                <View style={tw`w-[100%] border-[0.5px] border-[#D3D4D3]`}></View>
+                <Pressable onPress={onPressSave}><Text style={tw`text-[#191919] text-sm my-[15px]`}>저장</Text></Pressable>
+            </View>
+        </Modal>
+    )
+}
+
+// 한줄평 입력 모듈
+// props: modalVisible, setModalVisible, shortReview, setFinalShortReview
+export const ShortReviewForm = (props) => {
+    const [shortReview, setShortReview] = useState(props.shortReview) // 한줄평
+    const [charCount, setCharCount] = useState(0); // 문자 수
+
+    const onShortReviewChange = (text) => {
+        if (text.length <= 50) { // 최대 50자 제한
+            setShortReview(text);
+            setCharCount(text.length);
+        }
+    }
+
+    const onPressSave = () => { // shortReview ReviewWrite에 넘겨주기
+        if (shortReview.trim() === "") return;
+        setShortReview(shortReview);
+        props.setFinalShortReview(shortReview);
+        props.setModalVisible(false);
+    }
+
+    return (
+        <Modal animationIn={"fadeIn"} animationOut={"fadeOut"} transparent={true} isVisible={props.modalVisible} hasBackdrop={true} backdropOpacity={0.5} onBackdropPress={() => props.setModalVisible(false)}>
+            <View style={tw`flex flex-col justify-around items-center self-center w-[300px] bg-[#FAFAFA] rounded-[15px]`}>
+                <TextInput style={tw`flex flex-wrap text-sm mt-[28px] self-start mx-[26px]`} defaultValue={shortReview} placeholder="한줄평을 입력해주세요" placeholderTextColor="#B6B6B6" onChangeText={onShortReviewChange} maxLength={50} multiline={true} value={shortReview}></TextInput>
+                <Text style={tw`text-[#B6B6B6] text-sm mb-[27px] self-end mr-[26px]`}>{charCount} / 50</Text>
+                <View style={tw`w-[100%] border-[0.5px] border-[#D3D4D3]`}></View>
+                <Pressable onPress={onPressSave}><Text style={tw`text-[#191919] text-sm my-[15px]`}>저장</Text></Pressable>
+            </View>
+        </Modal>
+    )
+}
+
+// 긴줄평 입력 모듈
+// props: modalVisible, setModalVisible, longReview, setFinalLongReview, isLongReviewSpoiler, setIsFinalLongReviewSpoiler
+export const LongReviewForm = (props) => {
+    const [isLongReviewSpoiler, setIsLongReviewSpoiler] = useState(props.isLongReviewSpoiler);
+    const [longReviewCheckRectangle, setLongReviewCheckRectangle] = useState(require('@images/rectangle.png'));
+
+    const [charCount, setCharCount] = useState(0); // 문자 수
+    const [longReview, setLongReview] = useState(props.longReview); // 긴줄평
+
+    const goBack = () => {
+        props.setFinalLongReview(longReview);
+        props.setIsFinalLongReviewSpoiler(isLongReviewSpoiler);
+        props.setModalVisible(false);
+    };
+
+    const onLongReviewChange = (text) => {
+        if (text.length <= 2500) { // 최대 2500자 제한
+            setLongReview(text);
+            setCharCount(text.length);
+        }
+    }
+
+    const checkLongReviewSpoiler = () => {
+        if (longReviewCheckRectangle === require('@images/rectangle.png')) {
+            setIsLongReviewSpoiler(true);
+            setLongReviewCheckRectangle(require('@images/rectangle_checked_with_border.png'));
+        } else {
+            setIsLongReviewSpoiler(false);
+            setLongReviewCheckRectangle(require('@images/rectangle.png'));
+        }
+    };
+
+    return (
+        <Modal animationIn={"fadeIn"} animationOut={"fadeOut"} transparent={true} isVisible={props.modalVisible} hasBackdrop={true} backdropOpacity={0.5} onBackdropPress={() => props.setModalVisible(false)}>
+            <SafeAreaView style={styles.container}>
+                <View style={tw`flex-row items-center justify-between mt-5 mb-[14px]`}>
+                    <Pressable onPress={goBack} style={tw`flex-row`}>
+                        <Image source={require('@images/chevron_left.png')} style={tw`ml-[20px] mr-[8px] w-[10px] h-[18px] tint-[#191919]`}></Image>
+                        <View style={tw`px-[20px]`}></View>
+                    </Pressable>
+                    <Text style={tw`text-[#191919] text-base font-medium`}>긴줄평 적기</Text>
+                    <View style={tw`flex-row`}>
+                        <Image source={require('@images/chevron_left.png')} style={tw`ml-[20px] mr-[8px] w-[10px] h-[18px] tint-[#FAFAFA]`}></Image>
+                        <View style={tw`px-[20px]`}></View>
+                    </View>
+                </View>
+                <View style={tw`border-solid border-b border-[#D3D4D3]`}></View>
+
+                <View style={tw`flex-row items-center justify-between mt-[24px]`}>
+                    <View style={tw`flex-row items-center self-start ml-[5%]`}>
+                        <Pressable onPress={checkLongReviewSpoiler}>
+                            <Image source={longReviewCheckRectangle} style={tw`mr-2 w-[16px] h-[16px]`}></Image>
+                        </Pressable>
+                        <Text style={tw`text-[#B6B6B6] text-xs`}>스포일러 포함</Text>
+                    </View>
+                    <Text style={tw`text-[#B6B6B6] text-xs mb-[21px] self-end mr-[5%]`}>{charCount} / 2500</Text>
+                </View>
+                
+                <View style={tw`flex flex-row justify-between bg-[#F5F5F5] w-[90%] h-[80%] self-center rounded-[5px]`}>
+                    <TextInput style={tw`flex flex-wrap text-sm self-start mx-[26px] my-[15px]`} defaultValue={longReview} placeholder="긴줄평을 입력해주세요" placeholderTextColor="#B6B6B6" onChangeText={onLongReviewChange} maxLength={2500} multiline={true} value={longReview}></TextInput>
+                </View>
+            </SafeAreaView>
+        </Modal>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#FAFAFA',
+        width: '100%',
+        height: '100%',
+        borderRadius: 15,
+    },
+});
+
+
--- src/screens/Main/ReviewWrite1.js
+++ src/screens/Main/ReviewWrite1.js
@@ -1,7 +1,7 @@
-// 리뷰 작성용 페이지 (수정용 페이지는 따로 만들 예정, 구조는 동일하지만 정보 채워져 있음)
+// 리뷰 작성용 페이지 (수정용 페이지는 따로 만들 예정, 구조는 동일하지만 초기 상태에 정보 채워져 있음)
 
-import React, { useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet, Pressable, Alert, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, Pressable, ScrollView } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import tw from 'twrnc';
@@ -12,6 +12,8 @@ import { ChooseYearForm, ChooseMonthForm, ChooseDayForm } from '@forms/ChoosingD
 import MakeStarReviewForm from '@forms/MakeStarReviewForm';
 import AlertForm from '@forms/AlertForm';
 
+import { CastingForm, SeatForm, ShortReviewForm, LongReviewForm } from '@forms/ReviewContentsForm';
+
 export default function ReviewWrite1({isCookie, musicalId, musicalPoster, musicalTitle}) {
     const nav = useNavigation();
 
@@ -21,6 +23,10 @@ export default function ReviewWrite1({isCookie, musicalId, musicalPoster, musica
 
     // 추가 정보
     const [isExtraInfoExpanded, setIsExtraInfoExpanded] = useState(false);
+    const [castingModalVisible, setCastingModalVisible] = useState(false); // 캐스팅 모달
+    const [finalCastings, setFinalCastings] = useState([]); // 캐스팅 최종 리스트
+    const [seatModalVisible, setSeatModalVisible] = useState(false); // 좌석 모달
+    const [finalSeat, setFinalSeat] = useState(""); // 좌석 최종
 
     // 관람일자
     const [year, setYear] = useState(0);
@@ -31,12 +37,31 @@ export default function ReviewWrite1({isCookie, musicalId, musicalPoster, musica
     const [star, setStar] = useState(0);
 
     // 한줄평
-    const [shortReview, setShortReview] = useState('');
     const [isShortReviewSpoiler, setIsShortReviewSpoiler] = useState(false);
     const [shortReviewCheckRectangle, setShortReviewCheckRectangle] = useState(require('@images/rectangle.png'));
+    const [shortReviewModalVisible, setShortReviewModalVisible] = useState(false); // 한줄평 모달
+    const [shortReview, setShortReview] = useState(''); // 한줄평 최종
 
     // 긴줄평
+    const [longReviewModalVisible, setLongReviewModalVisible] = useState(false); // 긴줄평 모달
     const [longReview, setLongReview] = useState('');
+    const [isLongReviewSpoiler, setIsLongReviewSpoiler] = useState(false);
+
+    const onPressCasting = () => {
+        setCastingModalVisible(!castingModalVisible);
+    };
+
+    const onPressSeat = () => {
+        setSeatModalVisible(!seatModalVisible);
+    };
+
+    const onPressShortReview = () => {
+        setShortReviewModalVisible(!shortReviewModalVisible);
+    };
+
+    const onPressLongReview = () => {
+        setLongReviewModalVisible(!longReviewModalVisible);
+    };
 
     const checkShortReviewSpoiler = () => {
         if (shortReviewCheckRectangle === require('@images/rectangle.png')) {
@@ -53,7 +78,7 @@ export default function ReviewWrite1({isCookie, musicalId, musicalPoster, musica
     };
 
     const onPressSave = () => {
-        if (year === 0 || month === 0 || day === 0) {
+        if (year === 0 || month === 0 || day === 0) { // TODO: 관람일자를 현재 날짜로 설정하면 이 부분에 안 걸림
             setModalVisible(!modalVisible);
             setAlertImage(require('@images/x_red.png'));
             setAlertText('관람일자를 입력해주세요');
@@ -81,7 +106,7 @@ export default function ReviewWrite1({isCookie, musicalId, musicalPoster, musica
             return;
         }
 
-        console.log(`${year}년 ${month}월 ${day}일 관람, ${star}점, ${isShortReviewSpoiler ? '스포' : '비스포'}일러, ${shortReview}`);
+        console.log(`${finalCastings} 캐스팅, ${finalSeat} 좌석, ${year}년 ${month}월 ${day}일 관람, ${star}점, ${isShortReviewSpoiler ? '스포' : '비스포'}일러, ${shortReview}, ${isLongReviewSpoiler ? '스포' : '비스포'}일러, ${longReview}`);
         // 백 연결 구현
         // AlertForm "저장되었습니다" 띄우기
         // 그 다음 어디로 가지?
@@ -92,7 +117,7 @@ export default function ReviewWrite1({isCookie, musicalId, musicalPoster, musica
             <View>
                 <AlertForm modalVisible={modalVisible} setModalVisible={setModalVisible} borderColor="#F5F8F5" bgColor="#F5F8F5" image={alertImage} textColor="#191919" text={alertText}></AlertForm>
             </View>
-            
+
             <View style={tw`flex-row items-center justify-between mt-5 mb-[14px]`}>
                 <Pressable onPress={goBack} style={tw`flex-row`}>
                     <Image source={require('@images/chevron_left.png')} style={tw`ml-[20px] mr-[8px] w-[10px] h-[18px] tint-[#191919]`}></Image>
@@ -111,7 +136,6 @@ export default function ReviewWrite1({isCookie, musicalId, musicalPoster, musica
                 <Text style={tw`text-[#000] text-base font-medium leading-[25px]`}>{musicalTitle}</Text>
             </View>
 
-            <ScrollView>
             <View style={tw`border-4 border-[#F0F0F0] mb-[15px]`}></View>
             {!isExtraInfoExpanded ?
                 <>
@@ -125,15 +149,29 @@ export default function ReviewWrite1({isCookie, musicalId, musicalPoster, musica
                     <View style={tw`mx-[5%]`}>
                         <View style={tw`flex-row items-center mt-[3px] mb-[19px]`}>
                             <Text style={tw`text-[#191919] text-sm w-[15%]`}>캐스팅</Text>
-                            <Pressable onPress={()=>Alert.alert("캐스팅 추가")}>
-                                <Image source={require('@images/add_button.png')} style={tw`w-[20px] h-[20px]`}></Image>
+                            <Pressable onPress={onPressCasting}>
+                                <Image source={require('@images/add_button.png')} style={tw`w-[20px] h-[20px] mr-[48px]`}></Image>
                             </Pressable>
+                            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} style={tw`flex-row`}>
+                                {finalCastings.map((cast, index) => (
+                                    <View key={index} style={tw`flex-row mr-[4px] items-center bg-[#F5F5F5] h-[28px] rounded-[5px]`}>
+                                        <Text style={tw`text-[#191919] text-xs mx-[15px]`}>{cast}</Text>
+                                    </View>
+                                ))}
+                            </ScrollView>
+                            <CastingForm modalVisible={castingModalVisible} setModalVisible={setCastingModalVisible} castings={finalCastings} setFinalCastings={setFinalCastings} />
                         </View>
                         <View style={tw`flex-row items-center mb-[19px]`}>
                             <Text style={tw`text-[#191919] text-sm w-[15%]`}>좌석</Text>
-                            <Pressable onPress={()=>Alert.alert("좌석 추가")}>
-                                <Image source={require('@images/add_button.png')} style={tw`w-[20px] h-[20px]`}></Image>
+                            <Pressable onPress={onPressSeat}>
+                                <Image source={require('@images/add_button.png')} style={tw`w-[20px] h-[20px] mr-[48px]`}></Image>
                             </Pressable>
+                            {finalSeat !== "" ?
+                                <View style={tw`flex-row items-center bg-[#F5F5F5] h-[28px] rounded-[5px]`}>
+                                    <Text style={tw`text-[#191919] text-xs mx-[15px]`}>{finalSeat}</Text>
+                                </View>
+                            : null}
+                            <SeatForm modalVisible={seatModalVisible} setModalVisible={setSeatModalVisible} seat={finalSeat} setFinalSeat={setFinalSeat} />
                         </View>
                     </View>
                     <Pressable onPress={() => { setIsExtraInfoExpanded(!isExtraInfoExpanded); }} style={tw`flex-row items-center justify-center`}>
@@ -174,11 +212,12 @@ export default function ReviewWrite1({isCookie, musicalId, musicalPoster, musica
                 <View style={tw`w-[30%]`}>
                     <Text style={tw`text-[#191919] text-sm`}>한줄평 적기</Text>
                 </View>
-                <Pressable onPress={()=>Alert.alert("한줄평 작성 모듈")} style={tw`border-solid border-b-[1px] border-[#CCCCCC] flex-1 justify-between flex-row`}>
+                <Pressable onPress={onPressShortReview} style={tw`border-solid border-b-[1px] border-[#CCCCCC] flex-1 justify-between flex-row`}>
                     <Text style={tw`text-[#B6B6B6] text-sm mb-[10px]`}>“</Text>
-                    <Text style={tw`mx-[8px]`}>{shortReview}</Text>
+                    <Text style={tw`mx-[8px] mb-[7px] leading-[24px]`}>{shortReview}</Text>
                     <Text style={tw`text-[#B6B6B6] text-sm mb-[10px]`}>”</Text>
                 </Pressable>
+                <ShortReviewForm modalVisible={shortReviewModalVisible} setModalVisible={setShortReviewModalVisible} shortReview={shortReview} setFinalShortReview={setShortReview} />
             </View>
             <View style={tw`flex-row items-center self-start mx-[5%] mt-4`}>
                 <View style={tw`w-[30%]`}></View>
@@ -187,17 +226,15 @@ export default function ReviewWrite1({isCookie, musicalId, musicalPoster, musica
                 </Pressable>
                 <Text style={tw`text-[#B6B6B6] text-xs`}>스포일러 포함</Text>
             </View>
-            </ScrollView>
 
-            <View style={tw`absolute bottom-[30px] w-[90%] h-[33px] border-solid self-center`}>
-            <Pressable onPress={()=>Alert.alert("긴줄평 적기 페이지로 이동")}>
-                <View style={tw`w-[100%] h-[100%] shadow-sm rounded-[24px] bg-[#FFF] justify-center items-center`}>
-                    <Text style={tw`text-[#191919] text-xs`}>긴줄평 적기</Text>
-                </View>
-            </Pressable>
+            <View style={tw`absolute bottom-[20px] w-[90%] h-[33px] border-solid self-center`}>
+                <Pressable onPress={onPressLongReview}>
+                    <View style={tw`w-[100%] h-[100%] shadow-sm rounded-[24px] bg-[#FFF] justify-center items-center`}>
+                        <Text style={tw`text-[#191919] text-xs`}>긴줄평 적기</Text>
+                    </View>
+                </Pressable>
+                <LongReviewForm modalVisible={longReviewModalVisible} setModalVisible={setLongReviewModalVisible} longReview={longReview} setFinalLongReview={setLongReview} isLongReviewSpoiler={isLongReviewSpoiler} setIsFinalLongReviewSpoiler={setIsLongReviewSpoiler} />
             </View>
-            
-
         </SafeAreaView>
     )
 }
