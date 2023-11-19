55826f0049638e65291794d7d265fa739e77430b
Sean <seahnnnnnn@gmail.com>
2023-11-19 17:26:52 +0900

    feat: 튜토리얼 수정

--- src/screens/Main/Tutorial1.js
+++ src/screens/Main/Tutorial1.js
@@ -8,11 +8,11 @@ import tw from 'twrnc';
 // props: modalVisible, setModalVisible
 export const TutorialModal1 = (props) => {
     return (
-        <SafeAreaView style={styles.container}>
-            <Modal animationIn="slideInUp" animationOut="slideOutDown" transparent={true} isVisible={props.modalVisible} hasBackdrop={true} backdropOpacity={0.8} onBackdropPress={()=>props.setModalVisible(false)}>
-                    <Pressable onPress={()=>props.setModalVisible(false)}><Image source={require("@images/tutorial.png")} style={tw`w-full h-full`}></Image></Pressable>
-            </Modal>
-        </SafeAreaView>
+        <Modal animationIn="slideInUp" animationOut="slideOutDown" transparent={true} isVisible={props.modalVisible} hasBackdrop={true} backdropOpacity={0.8} onBackdropPress={()=>props.setModalVisible(false)}>
+            <SafeAreaView style={styles.container}>
+                <Pressable onPress={()=>props.setModalVisible(false)}><Image source={require("@images/tutorial.png")} style={tw`w-full h-full`}></Image></Pressable>
+            </SafeAreaView>
+        </Modal>
     )
 }
 
