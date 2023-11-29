d0717839ddb9ba03bb2b7e746f9448dba6194eeb
eojin03 <elena1014@naver.com>
2023-11-29 20:51:27 +0900

    fix: 사용자 경향 else 처리 수정

--- src/forms/UserTendency.js
+++ src/forms/UserTendency.js
@@ -42,7 +42,7 @@ export default function UserTendency(props) {
         } else if (maxMiddle < maxUpper && maxMiddle < maxLower || maxUpper === maxLower && maxUpper > maxMiddle) {
           setName('미식가 관객');
           setDescription('좋은 작품에는 그만한 이유가 있다');
-        } else if (maxLower < maxUpper && maxLower < maxMiddle || maxLower === maxUpper && maxLower === maxMiddle || maxUpper === maxMiddle && maxUpper > maxLower) {
+        } else {
           setName('박애주의자 관객');
           setDescription('세상에 좋은 작품이 너무 많아');
         }
@@ -55,10 +55,10 @@ export default function UserTendency(props) {
         } else if (maxMiddle < maxUpper && maxMiddle < maxLower || maxUpper === maxLower && maxUpper > maxMiddle) {
           setName('신중한 관객');
           setDescription('나만의 취향을 만들어가는 중');
-        } else if (maxLower < maxUpper && maxLower < maxMiddle || maxUpper === maxMiddle && maxUpper > maxLower) {
+        } else {
           setName('신중한 관객');
           setDescription('균형잡힌 시선으로 작품을 보는 평가자');
-        }
+        } 
       {/*C 극단적이다*/}
       } else if (upperPercentage >= 0.33 && lowerPercentage >= 0.33) {
         // Update state based on conditions for C
@@ -68,7 +68,7 @@ export default function UserTendency(props) {
         } else if (maxMiddle < maxUpper && maxMiddle < maxLower || maxMiddle === maxUpper && maxMiddle === maxLower || maxUpper === maxLower && maxUpper > maxMiddle) {
           setName('소신 있는 관객');
           setDescription('내 평가에 망설임은 없다');
-        } else if (maxLower < maxUpper && maxLower < maxMiddle) {
+        } else {
           setName('소신 있는 관객');
           setDescription('호불호가 명확한 편');
         }
@@ -81,13 +81,10 @@ export default function UserTendency(props) {
         } else if (maxMiddle < maxUpper && maxMiddle < maxLower || maxUpper === maxLower && maxUpper > maxMiddle) {
           setName('날카로운 관객');
           setDescription('좋아하는 포인트가 확실한 편');
-        } else if (maxLower < maxUpper && maxLower < maxMiddle || maxUpper === maxMiddle && maxUpper > maxLower) {
+        } else {
           setName('날카로운 관객');
           setDescription('매의 눈으로 작품을 보는 평가자');
         }
-      } else {
-        setName('평가자');
-        setDescription('이런저런 작품을 평가하는');
       }
     }
   }, [name, description, props.totalReviewCount]);
