47cca8556d1eeb9a68559eca9540c7e377b23682
Sean <seahnnnnnn@gmail.com>
2024-01-16 01:37:48 +0900

    feat: 2024년 추가

--- src/forms/ChoosingDateForm.js
+++ src/forms/ChoosingDateForm.js
@@ -4,7 +4,7 @@ import { View, Text, Image, Pressable, ScrollView } from 'react-native';
 import tw from 'twrnc';
 
 export function ChooseYearForm({setYear, year_}) {
-    const years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995];
+    const years = [2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999, 1998, 1997, 1996, 1995];
     const [isExpanded, setIsExpanded] = useState(false);
     if (year_ === undefined) {
         year_ = 0;
