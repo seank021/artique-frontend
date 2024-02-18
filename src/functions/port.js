a7d874b2cb0aca0628cba359f1300aa8e4f4edd2
Sean <seahnnnnnn@gmail.com>
2024-02-18 11:20:21 +0900

    feat: dev port setting, default to be dev port

--- src/functions/port.js
+++ src/functions/port.js
@@ -1,6 +1,6 @@
 export const getPort = () => {
-    const isDev = false;
-    const DEV_PORT = 3000;
+    const isDev = true;
+    const DEV_PORT = "http://3.39.145.210:8080";
     const PROD_PORT = "http://3.39.145.210";
 
     return isDev ? DEV_PORT : PROD_PORT;
