04880f3156a471a55f2181bfb322fee83065c467
Sean <seahnnnnnn@gmail.com>
2024-02-18 17:03:55 +0900

    fix: remove unnecessary import

--- src/screens/Main/Main1.js
+++ src/screens/Main/Main1.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Pressable, Text, StyleSheet, BackHandler, Alert } from "react-native";
+import { Pressable, Text, StyleSheet, BackHandler } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
 import tw from "twrnc";
@@ -29,7 +29,7 @@ export default function Main1 ({ isCookie }) {
     return (
         <SafeAreaView style={styles.container}>
             <AlertFormForConfirm modalVisible={exitModalVisible} setModalVisible={setExitModalVisible} question="앱을 종료하시겠습니까?" text='종료' onPress={BackHandler.exitApp} />
-            
+
             <Text>여기는 메인 페이지입니당</Text>
             <Pressable style={tw`flex-1 items-center justify-center`} onPress={goToFeed}>
                 <Text>Feed1으로 가기</Text>
