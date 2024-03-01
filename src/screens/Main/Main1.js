217fc6551003bc5bdb0e22512319e608d31bfc5a
Sean <seahn1021@snu.ac.kr>
2024-03-01 19:43:51 +0900

    feat: banner

--- src/screens/Main/Main1.js
+++ src/screens/Main/Main1.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { View, Pressable, Text, Image, StyleSheet, BackHandler, ScrollView } from "react-native";
+import { View, Pressable, Text, Image, StyleSheet, BackHandler, ScrollView, Linking } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
+import Swiper from "react-native-swiper";
 import tw from "twrnc";
 
 import { AlertFormForConfirm } from "@forms/AlertForm";
@@ -61,6 +62,26 @@ export default function Main1({ isCookie, setGoToFeed, setReviewId, setMusicalId
         { "musicalId": "PF150471", "musicalName": "빨간모자야 조심해! [청주, 세종]", "posterUrl": "http://www.kopis.or.kr/upload/pfmPoster/PF_PF150471_200629_135159.gif", starRating: 3.0, reviewCnt: 10 },
     ]
 
+    // type: 0(외부링크) - link: url, 1(내부뮤지컬) - link: musicalId, 2(내부리뷰) - link: reviewId
+    const banners = [
+        { "bannerimg": require("@images/bannerimg.png"), "type": 0, "link": "http://www.playdb.co.kr/artistdb/detail.asp?ManNo=27064" },
+        { "bannerimg": require("@images/bannerimg2.png"), "type": 0, "link": "https://litt.ly/artique" },
+        { "bannerimg": require("@images/bannerimg3.png"), "type": 1, "link": "PF222698" },
+        { "bannerimg": require("@images/bannerimg4.png"), "type": 2, "link": 68 },
+    ]
+
+    const openLink = (type, link) => {
+        if (type === 0) {
+            Linking.openURL(link);
+        } else if (type === 1) {
+            setMusicalId(link);
+            nav.navigate('MusicalDetail1');
+        } else if (type === 2) {
+            setReviewId(link);
+            nav.navigate('ReviewDetail1');
+        }
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <AlertFormForConfirm modalVisible={exitModalVisible} setModalVisible={setExitModalVisible} question="앱을 종료하시겠습니까?" text='종료' onPress={BackHandler.exitApp} />
@@ -68,7 +89,14 @@ export default function Main1({ isCookie, setGoToFeed, setReviewId, setMusicalId
             <Header isCookie={isCookie} onPressExit={onPressExit} />
 
             <ScrollView style={tw`flex flex-col`} showsVerticalScrollIndicator={false}>
-                <Image source={require("@images/bannerimg.png")} style={tw`w-[100%] h-[300px] mb-[30px]`}></Image>
+                <Swiper style={tw`h-[300px]`} autoplay={true} autoplayTimeout={5} showsPagination={true} dotColor="#BDBDBD" activeDotColor="#FF5C55">
+                    {banners.map((banner, index) => (
+                        <Pressable key={index} style={tw`flex flex-col items-center justify-center`} onPress={() => openLink(banner.type, banner.link)}>
+                            <Image source={banner.bannerimg} style={tw`w-[100%] h-[300px]`}></Image>
+                        </Pressable>
+                    ))}
+                </Swiper>
+                <View style={tw`h-[30px]`} />
 
                 <MainForm title="최신 리뷰" onPressMore={goToLatest} contents={latest} setReviewId={setReviewId} />
                 <MainForm title="공감 많은 리뷰" onPressMore={goToHeartest} contents={latest} setReviewId={setReviewId} />
