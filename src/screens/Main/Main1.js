d2f413734c88bf5cf87cbe73924db62922321706
Sean <seahn1021@snu.ac.kr>
2024-05-18 20:59:30 +0900

    fix: banner logic, refresh logic

--- src/screens/Main/Main1.js
+++ src/screens/Main/Main1.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { View, Pressable, Image, StyleSheet, BackHandler, ScrollView, Linking } from "react-native";
+import { View, Pressable, Image, StyleSheet, BackHandler, ScrollView, Linking, RefreshControl, ActivityIndicator } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useNavigation } from "@react-navigation/native";
+import { useIsFocused, useNavigation } from "@react-navigation/native";
 import Swiper from "react-native-swiper";
 import tw from "twrnc";
 
@@ -12,6 +12,9 @@ import { getHomeBanners, getHomeFiveStarReviews, getHomeLongReviews, getHomeRece
 
 export default function Main1({ isCookie, setGoToFeed, setReviewId, setMusicalId }) {
     const nav = useNavigation();
+
+    const [refreshing, setRefreshing] = useState(false);
+    const isFocused = useIsFocused();
 
     const [exitModalVisible, setExitModalVisible] = useState(false);
     
@@ -54,6 +57,9 @@ export default function Main1({ isCookie, setGoToFeed, setReviewId, setMusicalId
     const [banners, setBanners] = useState([]);
 
     useEffect(() => {
+        getHomeBanners().then((res) => {
+            setBanners(res.banners);
+        });
         getHomeRecentReviews().then((res) => {
             setLatest(res);
         });
@@ -69,10 +75,37 @@ export default function Main1({ isCookie, setGoToFeed, setReviewId, setMusicalId
         getHomeFiveStarReviews().then((res) => {
             setFiveStars(res);
         });
+    }, []);
+
+    useEffect(() => {
+        if (!isFocused) {
+            return;
+        }
+        onRefresh();
+    }, [isFocused]);
+
+    const onRefresh = () => {
+        setRefreshing(true);
         getHomeBanners().then((res) => {
             setBanners(res.banners);
         });
-    }, []);
+        getHomeRecentReviews().then((res) => {
+            setLatest(res);
+        });
+        getHomeThumbsReviews().then((res) => {
+            setThumbs(res);
+        });
+        getHomeRecommendMusicals().then((res) => {
+            setRecommends(res);
+        });
+        getHomeLongReviews().then((res) => {
+            setLongs(res);
+        });
+        getHomeFiveStarReviews().then((res) => {
+            setFiveStars(res);
+        });
+        setRefreshing(false);
+    }
 
     const openLink = (type, link) => {
         if (type === 0) {
@@ -86,22 +119,35 @@ export default function Main1({ isCookie, setGoToFeed, setReviewId, setMusicalId
         }
     }
 
+    if (banners[0] === undefined || banners[1] === undefined || banners[2] === undefined) {
+        return (
+            <SafeAreaView style={styles.container}>
+                <Header isCookie={isCookie} onPressExit={onPressExit} />
+                <ActivityIndicator size="large" color="#000000" style={{flex: 1, justifyContent: 'center', alignItems: 'center'}} />
+            </SafeAreaView>
+        )
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <AlertFormForConfirm modalVisible={exitModalVisible} setModalVisible={setExitModalVisible} question="앱을 종료하시겠습니까?" text='종료' onPress={BackHandler.exitApp} />
 
             <Header isCookie={isCookie} onPressExit={onPressExit} />
 
-            <ScrollView style={tw`flex flex-col`} showsVerticalScrollIndicator={false}>
-                <Swiper style={tw`h-[300px]`} autoplay={true} autoplayTimeout={5} showsPagination={true} dotColor="#BDBDBD" activeDotColor="#FF5C55">
-                    {banners && 
-                        banners.map((banner, index) => (
-                            <Pressable key={index} style={tw`flex flex-col items-center justify-center`} onPress={() => openLink(banner.type, banner.link)}>
-                                <Image source={{ uri: banner.bannerImg }} style={tw`w-[100%] h-[300px]`}></Image>
-                            </Pressable>
-                        ))
-                    }
-                </Swiper>
+            <ScrollView style={tw`flex flex-col`} showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+                {banners && banners[0] !== undefined && banners[1] !== undefined && banners[2] !== undefined &&
+                    <Swiper style={tw`h-[300px]`} showsPagination={true} dotColor="#BDBDBD" activeDotColor="#FF5C55">
+                        <Pressable style={tw`flex flex-col items-center justify-center`} onPress={() => openLink(banners[0].type, banners[0].link)}>
+                            <Image source={{ uri: banners[0].bannerImg }} style={tw`w-[100%] h-[300px]`}></Image>
+                        </Pressable>
+                        <Pressable style={tw`flex flex-col items-center justify-center`} onPress={() => openLink(banners[1].type, banners[1].link)}>
+                            <Image source={{ uri: banners[1].bannerImg }} style={tw`w-[100%] h-[300px]`}></Image>
+                        </Pressable>
+                        <Pressable style={tw`flex flex-col items-center justify-center`} onPress={() => openLink(banners[2].type, banners[2].link)}>
+                            <Image source={{ uri: banners[2].bannerImg }} style={tw`w-[100%] h-[300px]`}></Image>
+                        </Pressable>
+                    </Swiper>
+                }
                 <View style={tw`h-[30px]`} />
 
                 <MainForm title="최신 리뷰" onPressMore={goToLatest} contents={latest} setMusicalId={setMusicalId} setReviewId={setReviewId} />
